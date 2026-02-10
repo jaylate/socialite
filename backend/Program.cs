c4c1d9f2c2ca2e9d2cdb8177d721aa8d94d930f8
MaxAref07 <makar8777@gmail.com>
2026-02-10 20:21:59 +0100

    Backend: Fix API access

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,5 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -8,64 +9,10 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
-var api = app.MapGroup("/api/v1");
+//app.UseHttpsRedirection();
 
-// Posts endpoints
-api.MapGet("/posts", () =>
-{
-    return "TODO";
-});
+app.UseAuthorization();
 
-api.MapPost("/posts", () =>
-{
-    return "TODO";
-});
-
-api.MapGet("/posts/{id}", () =>
-{
-    return "TODO";
-});
-
-api.MapPut("/posts/{id}", () =>
-{
-    return "TODO";
-});
-
-api.MapDelete("/posts/{id}", () =>
-{
-    return "TODO";
-});
-
-api.MapPost("/posts/{id}/like", () =>
-{
-    return "TODO";
-});
-
-// User endpoints
-api.MapGet("/users", () =>
-{
-    return "TODO";
-});
-
-api.MapGet("/users/{id}", () =>
-{
-    return "TODO";
-});
-
-api.MapPut("/users/{id}", () =>
-{
-    return "TODO";
-});
-
-// Auth endpoints
-api.MapPost("/auth/login", () =>
-{
-    return "TODO";
-});
-
-api.MapPost("/auth/register", () =>
-{
-    return "TODO";
-});
+app.MapControllers();
 
 app.Run();
